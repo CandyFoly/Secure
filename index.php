0f927ed0652cbb5a89793e3a08f80ff5edf519f6
Mathilde Boucard <mathilde.boucard@outlook.fr>
2018-03-20 13:21:16 +0100

    Ajout index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php
 //include('controller/accueil_data.php')
 include('view/accueil_view.php')
-=======
-<?php
-//include('controller/accueil_data.php')
-include('view/accueil_view.php')
->>>>>>> 3e5d90d810f7933cb0a668185afd42203476e07f
 ?>
